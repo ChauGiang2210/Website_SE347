cfa6b16fa99a74ee719ff681d3fdb534d5807fbe
Lê Châu Giang <96758705+ChauGiang2210@users.noreply.github.com>
2023-12-11 23:32:58 +0700

    header

--- fe/yangthuk/src/assets/users/theme/header/index.js
+++ fe/yangthuk/src/assets/users/theme/header/index.js
@@ -1,7 +1,30 @@
 import { memo } from "react";
+import "./style.scss";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () =>
 {
-    return <h1>Header</h1>
+    return (
+    <div className="header_top">
+    <div>
+        <div className='row'>
+            <div className="col-1 header-top-left">
+            <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
+            </div>
+            <div className="col-2 header-top-right">
+            <ul>
+                <li><Link to={""}><IoSearch /></Link></li>
+                <li><Link to={""}><MdOutlineShoppingCart /></Link></li>
+                <li><Link to={""}><FaRegUser /></Link></li>
+            </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+    );
 };
 export default memo(Header);
--- fe/yangthuk/src/index.js
+++ fe/yangthuk/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import HomePage from './assets/users/HomePage';
 import RouterCustom from './router';
 import { BrowserRouter } from 'react-router-dom';
+import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
