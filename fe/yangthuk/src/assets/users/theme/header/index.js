b3c94c609d22f45fca37e8a38343bf3324e95b29
Lê Châu Giang <96758705+ChauGiang2210@users.noreply.github.com>
2023-12-15 22:53:12 +0700

    header-footer-homepage

--- fe/yangthuk/src/assets/users/theme/header/index.js
+++ fe/yangthuk/src/assets/users/theme/header/index.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo,useState,useEffect } from "react";
 import "./style.scss";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
@@ -8,36 +8,118 @@ import { Link } from "react-router-dom";
 
 const Header = () =>
 {
+    const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    setIsScrolled(scrollPosition > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
     return (
     <div>
     <div className="header_top">
-    <div>
+    <div className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
         <div className='row'>
             <div className="col-1 header-top-left">
             <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
             </div>
             <div className="col-2 header-top-right">
             <ul>
-                <li><Link to={""}><IoSearch /></Link></li>
-                <li><Link to={""}><MdOutlineShoppingCart /></Link></li>
-                <li><Link to={""}><FaRegUser /></Link></li>
+            <div className="search">
+            <form >
+              <input
+                type="text"
+                name="search"
+                placeholder="Tìm kiếm ..."
+                //defaultValue={setSearch}
+               // onChange={(e) => setSearch(e.target.value)}
+              ></input>
+              
+              {/* <button type="submit" onClick={(e) => SearchProduct(e)}>Search</button> */}
+            </form>
+            
+        </div>
+        {/* <li><IoSearch className="iosearch"/></li> */}
+                {/* <li ><Link to={""} className="btn">< IoSearch className='icon' /></Link></li> */}
+                <li><Link to={""} className="btn"><MdOutlineShoppingCart className='icon' /></Link></li>
+                <li><Link to={""} className="btn"><FaRegUser className='icon' /></Link></li>
             </ul>
             </div>
         </div>
     </div>
+
     </div >
+    <div className="head-content">
     <div className="Head-Image container">
         <img className='mp' src={process.env.PUBLIC_URL + '/mypham.jpg'} alt="Logo" />
     </div>
     <div className="move-image">
-    <marquee behavior="alternate" width="100%" loop="-1" direction="right">
-        <img className='move' src={process.env.PUBLIC_URL + '/move1.jpg'} alt="Logo" />
-        <img className='move' src={process.env.PUBLIC_URL + '/move2.jpg'} alt="Logo" />
-        <img className='move' src={process.env.PUBLIC_URL + '/move3.jpg'} alt="Logo" />
-        <img className='move' src={process.env.PUBLIC_URL + '/move4.jpg'} alt="Logo" />
-        <img className='move' src={process.env.PUBLIC_URL + '/move5.jpg'} alt="Logo" />
-        <img className='move' src={process.env.PUBLIC_URL + '/move6.jpg'} alt="Logo" />
-    </marquee>
+        <marquee
+          behavior="alternate"
+          width="100%"
+          loop="-1"
+          direction="right"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+        <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move1.jpg"}
+            id="move1"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move2.jpg"}
+            id="move2"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move3.jpg"}
+            id="move3"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move4.jpg"}
+            id="move4"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move5.jpg"}
+            id="move1"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move6.jpg"}
+            id="move2"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move7.jpeg"}
+            id="move2"
+          />
+          <img
+            className={`move ${isHovered ? "zoomed" : ""}`}
+            src={process.env.PUBLIC_URL + "/move8.jpg"}
+            id="move2"
+          />
+          {/* ... (Các ảnh khác trong marquee) */}
+        </marquee>
+    </div>
     </div>
     </div>
     );
